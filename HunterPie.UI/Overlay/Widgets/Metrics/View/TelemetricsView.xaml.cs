cfbba6e29fe1ca05848c0b39f140249d132d1798
Haato3o <me@haato.dev>
2021-12-31 15:00:11 -0300

    Feat(UI): Added Telemetrics Call GC button

--- HunterPie.UI/Overlay/Widgets/Metrics/View/TelemetricsView.xaml.cs
+++ HunterPie.UI/Overlay/Widgets/Metrics/View/TelemetricsView.xaml.cs
@@ -2,6 +2,7 @@
 using HunterPie.Core.Client.Configuration.Overlay;
 using HunterPie.UI.Architecture;
 using HunterPie.UI.Overlay.Widgets.Metrics.ViewModel;
+using System;
 
 namespace HunterPie.UI.Overlay.Widgets.Metrics.View
 {
@@ -18,5 +19,10 @@ namespace HunterPie.UI.Overlay.Widgets.Metrics.View
         public TelemetricsWidgetConfig Settings => ClientConfig.Config.Overlay.DebugWidget;
 
         public string Title => "Debug Metrics";
+
+        private void OnGCClick(object sender, EventArgs e)
+        {
+            ViewModel.ExecuteGarbageCollector();
+        }
     }
 }
