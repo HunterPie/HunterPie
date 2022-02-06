a63d5bfb33108441b9d7feb9b941895689664ae3
Haato3o <me@haato.dev>
2022-02-06 15:31:42 -0300

    Feat(Core): Mapped more Song buffs

--- HunterPie.UI/Settings/Internal/AbnormalityTraysVisualConverter.cs
+++ HunterPie.UI/Settings/Internal/AbnormalityTraysVisualConverter.cs
@@ -1,12 +1,7 @@
 ﻿using HunterPie.Core.Settings.Types;
 using HunterPie.UI.Controls.Settings.Custom;
 using HunterPie.UI.Settings.Converter;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace HunterPie.UI.Settings.Internal
