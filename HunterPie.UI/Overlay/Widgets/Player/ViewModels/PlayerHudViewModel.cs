11015be755253a643f5ef7eca43626bf457ef7eb
Haato3o <me@haato.dev>
2022-02-20 12:53:19 -0300

    Feat(UI): Added player health bar initial design

---
+++ HunterPie.UI/Overlay/Widgets/Player/ViewModels/PlayerHudViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HunterPie.Core.Architecture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterPie.UI.Overlay.Widgets.Player.ViewModels
+{
+    public class PlayerHudViewModel : Bindable
+    {
+    }
+}
