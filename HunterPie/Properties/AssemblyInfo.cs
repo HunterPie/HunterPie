3706c71f3e115ff8c848704d9985d470e99b66df
Haato3o <me@haato.dev>
2022-02-23 22:18:17 -0300

    Feat(Core): Added Support for 3.9.0.0

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.0.21.30")]
-[assembly: AssemblyFileVersion("2.0.21.30")]
+[assembly: AssemblyVersion("2.0.21.34")]
+[assembly: AssemblyFileVersion("2.0.21.34")]
