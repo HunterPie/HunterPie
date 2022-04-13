86d73321f0cc724a614d374c624098de15ba6d26
haato3o <me@haato.dev>
2022-04-12 22:34:42 -0300

    Refactor(Integrations): Refactored how HunterPie handles Discord Rich Presence

--- HunterPie/Integrations/Discord/RiseRichPresence.cs
+++ HunterPie/Integrations/Discord/RiseRichPresence.cs
@@ -1,92 +1,27 @@
 ﻿
 using DiscordRPC;
-using HunterPie.Core.Game;
 using HunterPie.Core.Game.Rise;
 using System;
 using System.Linq;
-using DiscordRPC.Message;
-using HunterPie.Core.Logger;
-using System.Timers;
 using HunterPie.Core.Game.Environment;
 using HunterPie.Core.Game.Enums;
-using HunterPie.Core.Client.Configuration.Integrations;
-using HunterPie.Core.Client;
-using System.ComponentModel;
 using HunterPie.Core.Client.Localization;
 
 namespace HunterPie.Integrations.Discord
 {
-    internal class RiseRichPresence : IDisposable
+    internal class RiseRichPresence : RichPresence
     {
-        const string AppId = "932399108017242182";
-        private MHRGame game;
-        private Timestamps locationTime = Timestamps.Now;
-        private readonly RichPresence presence = new();
-        private readonly DiscordRpcClient client = new(AppId, autoEvents: true);
-        private DiscordRichPresence Settings => ClientConfig.Config.RichPresence;
+        private const string RISE_APP_ID = "932399108017242182";
 
-        private readonly Timer timer = new(10000)
+        private readonly MHRGame game;
+
+        public RiseRichPresence(MHRContext context) : base(RISE_APP_ID, context.Game)
         {
-            AutoReset = true
-        };
-
-        public RiseRichPresence(Context context)
-        {
-            if (context.Game is not MHRGame)
-                return;
-
             game = (MHRGame)context.Game;
-
-            client.Initialize();
-            timer.Start();
-            HookEvents();
         }
 
-        private void HookEvents()
+        protected override void HandlePresence()
         {
-            client.OnReady += OnReady;
-            Settings.EnableRichPresence.PropertyChanged += OnEnableRichPresenceChanged;
-            game.Player.OnStageUpdate += OnStageUpdate;
-            timer.Elapsed += OnTick;
-        }
-
-        private void UnhookEvents()
-        {
-            client.OnReady -= OnReady;
-            game.Player.OnStageUpdate -= OnStageUpdate;
-            Settings.EnableRichPresence.PropertyChanged -= OnEnableRichPresenceChanged;
-            timer.Elapsed -= OnTick;
-        }
-
-        private void OnEnableRichPresenceChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (Settings.EnableRichPresence)
-                UpdatePresence();
-            else
-                client.ClearPresence();
-        }
-
-        private void OnReady(object sender, ReadyMessage args)
-        {
-            Log.Info($"Connected to Discord: {args.User}");
-            UpdatePresence();
-        }
-
-        private void OnTick(object sender, ElapsedEventArgs e) => UpdatePresence();
-        private void OnStageUpdate(object sender, EventArgs e)
-        {
-            locationTime = Timestamps.Now;
-            UpdatePresence();
-        }
-
-        private void UpdatePresence()
-        {
-            if (!Settings.EnableRichPresence || client is null || !client.IsInitialized)
-            {
-                client.ClearPresence();
-                return;
-            }
-
             string description = null;
 
             description = game.Player.StageId switch
@@ -102,7 +37,7 @@ namespace HunterPie.Integrations.Discord
             IMonster targetMonster = game.Monsters.FirstOrDefault(monster => monster.Target == Target.Self);
             if (targetMonster is not null)
             {
-                string descriptionString = Settings.ShowMonsterHealth 
+                string descriptionString = Settings.ShowMonsterHealth
                     ? Localization.QueryString("//Strings/Client/Integrations/Discord[@Id='DRPC_RISE_STATE_HUNTING']")
                     : Localization.QueryString("//Strings/Client/Integrations/Discord[@Id='DRPC_RISE_STATE_HUNTING_NO_HEALTH']");
 
@@ -115,7 +50,7 @@ namespace HunterPie.Integrations.Discord
                 ? Localization.QueryString("//Strings/Client/Integrations/Discord[@Id='DRPC_RISE_PARTY_STATE_SOLO_STRING']")
                 : Localization.QueryString("//Strings/Client/Integrations/Discord[@Id='DRPC_RISE_PARTY_STATE_GROUP_STRING']");
 
-            presence.WithDetails(description)
+            Presence.WithDetails(description)
                 .WithAssets(new Assets()
                 {
                     LargeImageText = MHRContext.Strings.GetStageNameById(game.Player.StageId),
@@ -141,19 +76,8 @@ namespace HunterPie.Integrations.Discord
                     Size = game.Player.Party.Size,
                     Privacy = Party.PrivacySetting.Public
                 })
-                .WithState(state)
-                .WithTimestamps(locationTime);
+                .WithState(state);
 
-            client.SetPresence(presence);
-        }
-
-        public void Dispose()
-        {
-            UnhookEvents();
-            client.ClearPresence();
-            client.Dispose();
-            timer.Stop();
-            timer.Dispose();
         }
     }
 }
