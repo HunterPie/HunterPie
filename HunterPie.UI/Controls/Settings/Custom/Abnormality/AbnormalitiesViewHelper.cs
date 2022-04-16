535cab1a16d3ed2306af47b8ddc3a8a661d38cbb
haato3o <me@haato.dev>
2022-04-16 00:56:55 -0300

    Feat(UI): Load abnormalities file based on chosen game

--- HunterPie.UI/Controls/Settings/Custom/Abnormality/AbnormalitiesViewHelper.cs
+++ HunterPie.UI/Controls/Settings/Custom/Abnormality/AbnormalitiesViewHelper.cs
@@ -6,8 +6,6 @@ using HunterPie.UI.Assets.Application;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Xml;
 
