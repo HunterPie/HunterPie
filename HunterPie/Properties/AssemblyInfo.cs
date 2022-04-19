a21e4dae4bea5fff33abff18ae735456a1e8c38c
haato3o <me@haato.dev>
2022-04-19 00:48:23 -0300

    Feat(Core): Added missing names for HH buffs

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.0.22.420")]
-[assembly: AssemblyFileVersion("2.0.22.420")]
+[assembly: AssemblyVersion("2.0.22.421")]
+[assembly: AssemblyFileVersion("2.0.22.421")]
