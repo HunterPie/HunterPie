55b8ee4b84df1de74f28c9e8615d4a1ea909ffe0
haato3o <me@haato.dev>
2022-04-20 02:36:04 -0300

    Fix(Core): Fixed debounce time to avoid file writes too fast

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.0.22.422")]
-[assembly: AssemblyFileVersion("2.0.22.422")]
+[assembly: AssemblyVersion("2.0.22.423")]
+[assembly: AssemblyFileVersion("2.0.22.423")]
