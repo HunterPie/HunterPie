b6418822fbcdf9957ff1f779b15f83075e975b43
haato3o <me@haato.dev>
2022-04-21 20:54:14 -0300

    Fix(UI): Fixed Settings Host to have scrollbar when settings selection overflows

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.0.22.446")]
-[assembly: AssemblyFileVersion("2.0.22.446")]
+[assembly: AssemblyVersion("2.0.22.455")]
+[assembly: AssemblyFileVersion("2.0.22.455")]
