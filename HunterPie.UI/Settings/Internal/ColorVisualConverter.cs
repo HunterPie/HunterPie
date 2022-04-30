88efc5e4d1de844f913f0d1080debbc3423ea5e7
haato3o <me@haato.dev>
2022-04-30 02:50:24 -0300

    Feat(UI): Add Color picker control

---
+++ HunterPie.UI/Settings/Internal/ColorVisualConverter.cs
@@ -0,0 +1,20 @@
+﻿using HunterPie.Core.Settings.Types;
+using HunterPie.UI.Controls.Buttons;
+using HunterPie.UI.Settings.Converter;
+using System.Reflection;
+using System.Windows;
+
+namespace HunterPie.UI.Settings.Internal
+{
+    public class ColorVisualConverter : IVisualConverter
+    {
+        public FrameworkElement Build(object parent, PropertyInfo childInfo)
+        {
+            Color viewModel = (Color)childInfo.GetValue(parent);
+            return new ColorPicker()
+            {
+                DataContext = viewModel,
+            };
+        }
+    }
+}
