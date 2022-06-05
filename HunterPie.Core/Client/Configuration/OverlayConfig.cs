143c692e949ca3be286b4bf9285e2bd692c7d4c4
haato3o <me@haato.dev>
2022-06-05 01:50:34 -0300

    Fix(Core): Fixed default values for Specialized Tool position and scale

--- HunterPie.Core/Client/Configuration/OverlayConfig.cs
+++ HunterPie.Core/Client/Configuration/OverlayConfig.cs
@@ -15,8 +15,15 @@ namespace HunterPie.Core.Client.Configuration
         public WirebugWidgetConfig WirebugWidget { get; set; } = new();
         public ActivitiesWidgetConfig ActivitiesWidget { get; set; } = new();
         public ChatWidgetConfig ChatWidget { get; set; } = new();
-        public SpecializedToolWidgetConfig PrimarySpecializedToolWidget { get; set; } = new();
-        public SpecializedToolWidgetConfig SecondarySpecializedToolWidget { get; set; } = new();
+        public SpecializedToolWidgetConfig PrimarySpecializedToolWidget { get; set; } = new()
+        {
+            Position = new(950, 400)
+        };
+        public SpecializedToolWidgetConfig SecondarySpecializedToolWidget { get; set; } = new()
+        {
+            Position = new(1000, 375),
+            Scale = new(0.9, 2, 0.1, 0.1)
+        };
         public Keybinding ToggleDesignMode { get; set; } = "ScrollLock";
 
         public Observable<bool> HideWhenUnfocus { get; set; } = false;
