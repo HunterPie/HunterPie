55176b1341ef7d2bce301d91aece7021e48293c2
haato3o <me@haato.dev>
2022-06-30 19:45:40 -0300

    Feat(Localization): Add Elgado stage names

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Windows;
 )]
 
 
-[assembly: AssemblyVersion("2.1.1.2")]
-[assembly: AssemblyFileVersion("2.1.1.2")]
+[assembly: AssemblyVersion("2.1.1.3")]
+[assembly: AssemblyFileVersion("2.1.1.3")]
 
