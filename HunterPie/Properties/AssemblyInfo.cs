25632cd72c25b061f26e2819efcf74831ed00795
haato3o <me@haato.dev>
2022-07-04 21:45:05 -0300

    v2.1.1 :rocket:

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Windows;
 )]
 
 
-[assembly: AssemblyVersion("2.1.1.127")]
-[assembly: AssemblyFileVersion("2.1.1.127")]
+[assembly: AssemblyVersion("2.1.1.129")]
+[assembly: AssemblyFileVersion("2.1.1.129")]
 
