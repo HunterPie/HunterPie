19dd20b80601114502fd375707d88eb5e6740090
haato3o <me@haato.dev>
2022-07-18 12:35:03 -0300

    Feat(Core): Log self-update possible crashes

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.2.0.292")]
-[assembly: AssemblyFileVersion("2.2.0.292")]
+[assembly: AssemblyVersion("2.2.0.293")]
+[assembly: AssemblyFileVersion("2.2.0.293")]
 
