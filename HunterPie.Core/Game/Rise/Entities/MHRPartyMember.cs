3d9a9be1eecaa22aba4eb66f84f0c3e9ef0eb748
haato3o <me@haato.dev>
2022-08-03 22:31:10 -0300

    Feat(UI): Mock new pet tracker

---
+++ HunterPie.Core/Game/Rise/Entities/MHRPartyMember.cs
@@ -0,0 +1,26 @@
+﻿using HunterPie.Core.Game.Client;
+using HunterPie.Core.Game.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HunterPie.Core.Game.Rise.Entities
+{
+    public class MHRPartyMember : IPartyMember
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int Damage => throw new NotImplementedException();
+
+        public Weapon Weapon => throw new NotImplementedException();
+
+        public int Slot => throw new NotImplementedException();
+
+        public bool IsMyself => throw new NotImplementedException();
+
+        public event EventHandler<IPartyMember> OnDamageDealt;
+        public event EventHandler<IPartyMember> OnWeaponChange;
+    }
+}
