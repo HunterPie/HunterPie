17e9149c2021ed559d54733c674a2022e0b59eb4
haato3o <me@haato.dev>
2022-08-24 01:22:20 -0300

    Fix(Core): Fixe party members respawning when switching weapons

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.3.0.120")]
-[assembly: AssemblyFileVersion("2.3.0.120")]
+[assembly: AssemblyVersion("2.3.0.126")]
+[assembly: AssemblyFileVersion("2.3.0.126")]
