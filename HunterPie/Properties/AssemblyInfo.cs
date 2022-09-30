7a5c3e49d849a4deb6d3cca14d11912741117863
haato3o <me@haato.dev>
2022-09-29 23:17:13 -0300

    Hotfix(Data): Fixed debuff offsets

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.4.0.232")]
-[assembly: AssemblyFileVersion("2.4.0.232")]
+[assembly: AssemblyVersion("2.4.0.233")]
+[assembly: AssemblyFileVersion("2.4.0.233")]
