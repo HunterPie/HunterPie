e98b55157ea8571ac699c9a085bf8d77e75ec6cf
Haato3o <me@haato.dev>
2022-10-08 12:45:27 -0300

    Refactor: Separate BackupView from preferences view

---
+++ HunterPie/GUI/Parts/Backup/Views/BackupsView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace HunterPie.GUI.Parts.Backup.Views;
+/// <summary>
+/// Interaction logic for BackupsView.xaml
+/// </summary>
+public partial class BackupsView : UserControl
+{
+    public BackupsView()
+    {
+        InitializeComponent();
+    }
+}
