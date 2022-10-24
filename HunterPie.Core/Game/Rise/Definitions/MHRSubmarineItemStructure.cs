99d4fb62afeb858d88939db1199fcad8350024e7
haato3o <me@haato.dev>
2022-10-24 01:47:15 -0300

    Fix(Core): Fixed submarine total item count miscalculation

--- HunterPie.Core/Game/Rise/Definitions/MHRSubmarineItemStructure.cs
+++ HunterPie.Core/Game/Rise/Definitions/MHRSubmarineItemStructure.cs
@@ -5,19 +5,7 @@ namespace HunterPie.Core.Game.Rise.Definitions;
 [StructLayout(LayoutKind.Explicit)]
 public struct MHRSubmarineItemStructure
 {
-    [FieldOffset(0x8)]
-    public int State;
-
-    [FieldOffset(0x40)]
-    public byte Unk0;
-
-    [FieldOffset(0x41)]
-    public byte Unk1;
-
-    [FieldOffset(0x42)]
-    public byte Unk2;
-
-    [FieldOffset(0x44)]
+    [FieldOffset(0x14)]
     public int Amount;
 
     public bool IsNotEmpty() => Amount > 0;
