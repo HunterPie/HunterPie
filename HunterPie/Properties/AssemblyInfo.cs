45658951ee6d24fb81b9ac58f517fbd2117ec036
haato3o <me@haato.dev>
2022-10-30 13:07:17 -0300

    Feat(UI): Added Bloodblight icon

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.5.0.17")]
-[assembly: AssemblyFileVersion("2.5.0.17")]
+[assembly: AssemblyVersion("2.5.0.18")]
+[assembly: AssemblyFileVersion("2.5.0.18")]
