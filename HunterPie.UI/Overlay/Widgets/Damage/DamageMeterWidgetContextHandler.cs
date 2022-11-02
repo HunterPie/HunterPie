965a8f91824c42c442fda299c7a4cd4c35ecb4d0
haato3o <me@haato.dev>
2022-11-02 14:48:22 -0300

    Debug: Add logging message for when timer resets

--- HunterPie.UI/Overlay/Widgets/Damage/DamageMeterWidgetContextHandler.cs
+++ HunterPie.UI/Overlay/Widgets/Damage/DamageMeterWidgetContextHandler.cs
@@ -153,6 +153,8 @@ public class DamageMeterWidgetContextHandler : IContextHandler
 
         if (isTimerReset)
         {
+            Log.Debug("Timer has reset");
+
             // If the timer has just been reset, it usually means the local timer is being replaced with real game timer.
             // Note the info of party members in the current hunting party can be loaded before the real game timer gets ready in MHW.
             // Use 0 sec as other player's join time in this case to prevent a very large dps result.
