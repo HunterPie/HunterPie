47e9d48f2b908ea9dc054abfd5bb6d58607b8551
haato3o <me@haato.dev>
2022-11-13 00:14:51 -0300

    Feat(UI): Optimizations to the Bar control

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.5.0.272")]
-[assembly: AssemblyFileVersion("2.5.0.272")]
+[assembly: AssemblyVersion("2.5.0.279")]
+[assembly: AssemblyFileVersion("2.5.0.279")]
