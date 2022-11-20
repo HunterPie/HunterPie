88cf4409f8b72150e96c53dd9aed4f0f96538a42
haato3o <me@haato.dev>
2022-11-20 05:02:07 -0300

    Test: Delete integration tests from core

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.6.0.68")]
-[assembly: AssemblyFileVersion("2.6.0.68")]
+[assembly: AssemblyVersion("2.6.0.71")]
+[assembly: AssemblyFileVersion("2.6.0.71")]
