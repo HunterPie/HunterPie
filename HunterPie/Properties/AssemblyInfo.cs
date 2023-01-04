847e23a9d25c0cab0e97c4db80706b7c91be5540
Matheus Rangel <me@haato.dev>
2023-01-04 20:24:21 -0300

    Hotfix(Core): Fixed maximum possible health (#340)

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.6.0.160")]
-[assembly: AssemblyFileVersion("2.6.0.160")]
+[assembly: AssemblyVersion("2.6.0.161")]
+[assembly: AssemblyFileVersion("2.6.0.161")]
