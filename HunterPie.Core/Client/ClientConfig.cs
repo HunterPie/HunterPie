8da6660bb2eb874bd08a8c54631759712f1a76e2
Matheus Rangel <me@haato.dev>
2023-02-01 23:54:14 -0300

    Fix(Core): Changed when client config is synced with remote's (#359)
    * Fix(Core): Fixed when the config.json is going to be synced with the server's
    
    * Hotfix(Core): Fixed switch scroll enum values (#358)
    
    * Hotfix(Core): Fixed maximum possible health
    
    * Merge main
    
    * Fix(Core): Fixed switch scroll values
    
    * Chore: Bump version

--- HunterPie.Core/Client/ClientConfig.cs
+++ HunterPie.Core/Client/ClientConfig.cs
@@ -3,28 +3,17 @@ using HunterPie.Core.Logger;
 
 namespace HunterPie.Core.Client;
 
-public class ClientConfig
+#nullable enable
+public static class ClientConfig
 {
     public const string CONFIG_NAME = "config.json";
 
-    private readonly V3Config _config = new();
-    private static ClientConfig _instance;
-
-    public static V3Config Config => _instance._config;
-
-    private ClientConfig()
-    {
-        _instance = this;
-        ConfigManager.Register(CONFIG_NAME, _config);
-
-        Log.Info("Initialized HunterPie Client configuration.");
-    }
+    public static V3Config Config { get; } = new();
 
     internal static void Initialize()
     {
-        if (_instance is not null)
-            return;
+        ConfigManager.Register(CONFIG_NAME, Config);
 
-        _ = new ClientConfig();
+        Log.Info("Initialized HunterPie Client configuration.");
     }
 }
--- HunterPie/Integrations/Poogie/PoogieProvider.cs
+++ HunterPie/Integrations/Poogie/PoogieProvider.cs
@@ -14,6 +14,7 @@ internal static class PoogieProvider
     private const string USER_AGENT = "User-Agent";
     private const string TOKEN = "X-Token";
     private const string SUPPORTER = "X-Supporter-Token";
+    private const string CLIENT_TYPE_VALUE = "v2";
 
     private static readonly string[] Hosts = { "https://api.hunterpie.com", "https://mirror.hunterpie.com/mirror" };
 
@@ -28,8 +29,8 @@ internal static class PoogieProvider
             FeatureFlags.FEATURE_REDIRECT_POOGIE
         );
 
-        HttpClientBuilder builder = shouldRedirect
-            ? new HttpClientBuilder(ClientConfig.Config.Development.PoogieApiHost)
+        HttpClientBuilder builder = shouldRedirect && ClientConfig.Config is { } config
+            ? new HttpClientBuilder(config.Development.PoogieApiHost)
             : new HttpClientBuilder(Hosts);
 
         string token = CredentialVaultService.GetCredential()?.Password;
@@ -37,9 +38,9 @@ internal static class PoogieProvider
         return builder.WithTimeout(TimeSpan.FromSeconds(10))
                       .WithRetry(3)
                       .WithHeader(CLIENT_ID, clientId)
-                      .WithHeader(SUPPORTER, ClientConfig.Config.Client.SupporterSecretToken)
+                      .WithHeader(SUPPORTER, ClientConfig.Config?.Client.SupporterSecretToken)
                       .WithHeader(APP_VERSION, ClientInfo.Version.ToString())
-                      .WithHeader(CLIENT_TYPE, "v2")
+                      .WithHeader(CLIENT_TYPE, CLIENT_TYPE_VALUE)
                       .WithHeader(USER_AGENT, GetUserAgent())
                       .WithHeader(TOKEN, token);
     }
