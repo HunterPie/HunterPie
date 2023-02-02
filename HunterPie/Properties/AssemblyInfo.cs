f921b22971e0165c453a4d60115a11c812275257
Matheus Rangel <me@haato.dev>
2023-02-01 23:39:40 -0300

    Hotfix(Core): Fixed switch scroll enum values (#358)
    * Hotfix(Core): Fixed maximum possible health
    
    * Merge main
    
    * Fix(Core): Fixed switch scroll values

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.6.0.163")]
-[assembly: AssemblyFileVersion("2.6.0.163")]
+[assembly: AssemblyVersion("2.6.0.164")]
+[assembly: AssemblyFileVersion("2.6.0.164")]
