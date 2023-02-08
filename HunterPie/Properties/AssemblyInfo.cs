a2b651a135dc6f4b34e09ca3931dcf4bb19d7d6a
haato3o <me@haato.dev>
2023-02-08 20:36:47 -0300

    Fix(Data): Fixed new monsters data

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.6.0.170")]
-[assembly: AssemblyFileVersion("2.6.0.170")]
+[assembly: AssemblyVersion("2.6.0.171")]
+[assembly: AssemblyFileVersion("2.6.0.171")]
