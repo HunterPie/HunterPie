2f9067eea8cc73bcddf894dbe27bc6b6f9ac55e0
WetU <50101064+WetU@users.noreply.github.com>
2023-02-11 03:52:27 +0900

    Feat(UI): Add support for Wirebug State in Wirebug Widget

---
+++ HunterPie.UI/Architecture/Converters/WirebugStateToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using HunterPie.Integrations.Datasources.MonsterHunterRise.Entity.Enums;
+using HunterPie.UI.Assets.Application;
+
+namespace HunterPie.UI.Architecture.Converters;
+
+public class WirebugStateToColorConverter : IValueConverter
+{
+    private readonly Dictionary<WirebugState, Color> _stateColors = new(6)
+    {
+        { WirebugState.None, Resources.Get<Color>("WIREBUG_NORMAL") },
+        { WirebugState.Blocked, Resources.Get<Color>("WIREBUG_NORMAL") },
+        { WirebugState.IceBlight, Resources.Get<Color>("WIREBUG_ICEBLIGHT") },
+        { WirebugState.WindMantle, Resources.Get<Color>("WIREBUG_WINDMANTLE") },
+        { WirebugState.RubyWirebug, Resources.Get<Color>("WIREBUG_RUBY") },
+        { WirebugState.GoldWirebug, Resources.Get<Color>("WIREBUG_GOLD") }
+    };
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is WirebugState wirebugState)
+        {
+            Color color = _stateColors[wirebugState];
+            return color;
+        }
+
+        throw new ArgumentException("value must be wirebugState");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
---
+++ HunterPie.UI/Architecture/Converters/WirebugStateToIconConverter.cs
@@ -0,0 +1,36 @@
+﻿using HunterPie.Integrations.Datasources.MonsterHunterRise.Entity.Enums;
+using HunterPie.UI.Assets.Application;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HunterPie.UI.Architecture.Converters;
+
+public class WirebugStateToIconConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        string iconName = null;
+
+        switch ((WirebugState)value)
+        {
+            case WirebugState.IceBlight:
+                iconName = "ICON_WIREBUG_ICEBLIGHT";
+                break;
+            case WirebugState.WindMantle:
+                iconName = "ICON_WIREBUG_WINDMANTLE";
+                break;
+            case WirebugState.RubyWirebug:
+                iconName = "ICON_WIREBUG_RUBY";
+                break;
+            case WirebugState.GoldWirebug:
+                iconName = "ICON_WIREBUG_GOLD";
+                break;
+            default:
+                return Resources.Icon("ICON_WIREBUG");
+        }
+
+        return iconName is null ? Resources.Icon("ICON_WIREBUG") : Resources.Icon(iconName);
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
