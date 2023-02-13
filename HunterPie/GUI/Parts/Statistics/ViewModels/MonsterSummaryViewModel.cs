3de91c925e178288c800f2713c9068f5f21a9799
haato3o <me@haato.dev>
2023-02-13 01:35:57 -0300

    Feat(UI): Added basic UI components

---
+++ HunterPie/GUI/Parts/Statistics/ViewModels/MonsterSummaryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using HunterPie.Core.Client.Configuration.Enums;
+using HunterPie.UI.Architecture;
+
+namespace HunterPie.GUI.Parts.Statistics.ViewModels;
+internal class MonsterSummaryViewModel : ViewModel
+{
+    private GameType _gameType;
+    public GameType GameType
+    {
+        get => _gameType;
+        set => SetValue(ref _gameType, value);
+    }
+
+    private int _id;
+    public int Id
+    {
+        get => _id;
+        set => SetValue(ref _id, value);
+    }
+}
