0cfd73c49cc0e33275f099f3cf10b5ba20fc1ab7
haato3o <me@haato.dev>
2023-02-20 03:41:18 -0300

    Feat(Core): Added cache for the hunt summaries connector

---
+++ HunterPie.Core/Domain/Cache/InMemoryAsyncCache.cs
@@ -0,0 +1,60 @@
+﻿using HunterPie.Core.Domain.Cache.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HunterPie.Core.Domain.Cache;
+
+#nullable enable
+/// <summary>
+/// A thread safe and asynchronous in-memory cache
+/// </summary>
+public class InMemoryAsyncCache : IAsyncCache
+{
+    private readonly SemaphoreSlim _semaphore = new(1);
+    private readonly Dictionary<string, ThreadSafeCacheEntry> _innerCache = new(50);
+
+    public async Task<T?> Get<T>(string key)
+    {
+        try
+        {
+            await _semaphore.WaitAsync();
+
+            bool exists = _innerCache.TryGetValue(key, out ThreadSafeCacheEntry value);
+
+            if (!exists)
+                return default;
+
+            return value is T typedValue ? typedValue : default;
+        }
+        catch
+        {
+            return default;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async Task Set<T>(string key, T value, CacheOptions? options = null)
+    {
+        options ??= CacheOptions.Default;
+
+        try
+        {
+            await _semaphore.WaitAsync();
+
+            _innerCache[key] = new ThreadSafeCacheEntry(
+                ExpiresAt: DateTime.UtcNow + options.Ttl,
+                Value: value
+            );
+        }
+        catch { }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
--- HunterPie/Integrations/Poogie/Statistics/PoogieStatisticsConnector.cs
+++ HunterPie/Integrations/Poogie/Statistics/PoogieStatisticsConnector.cs
@@ -1,12 +1,23 @@
-﻿using HunterPie.Integrations.Poogie.Common;
+﻿using HunterPie.Core.Domain.Cache;
+using HunterPie.Core.Domain.Cache.Model;
+using HunterPie.Integrations.Poogie.Common;
 using HunterPie.Integrations.Poogie.Common.Models;
 using HunterPie.Integrations.Poogie.Statistics.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace HunterPie.Integrations.Poogie.Statistics;
+#nullable enable
 internal class PoogieStatisticsConnector
 {
+    private const string SUMMARIES_CACHE_KEY = "summaries";
+
+    /// <summary>
+    /// Common cache for all instances of this connector
+    /// </summary>
+    private static readonly IAsyncCache Cache = new InMemoryAsyncCache();
+
     private readonly PoogieConnector _connector = new();
 
     private const string STATISTICS_ENDPOINT = "/v1/hunt";
@@ -14,6 +25,24 @@ internal class PoogieStatisticsConnector
     public async Task<PoogieResult<PoogieQuestStatisticsModel>> Upload(PoogieQuestStatisticsModel model) =>
         await _connector.Post<PoogieQuestStatisticsModel, PoogieQuestStatisticsModel>($"{STATISTICS_ENDPOINT}/upload", model);
 
-    public async Task<PoogieResult<List<PoogieQuestSummaryModel>>> GetUserQuestSummaries() =>
-        await _connector.Get<List<PoogieQuestSummaryModel>>($"{STATISTICS_ENDPOINT}");
+    public async Task<PoogieResult<List<PoogieQuestSummaryModel>>> GetUserQuestSummaries()
+    {
+        PoogieResult<List<PoogieQuestSummaryModel>>? cachedSummaries =
+            await Cache.Get<PoogieResult<List<PoogieQuestSummaryModel>>>(SUMMARIES_CACHE_KEY);
+
+        if (cachedSummaries is { } cached)
+            return cached;
+
+        PoogieResult<List<PoogieQuestSummaryModel>> result = await _connector.Get<List<PoogieQuestSummaryModel>>($"{STATISTICS_ENDPOINT}");
+
+        if (result.Response is not null)
+            await Cache.Set(
+                key: SUMMARIES_CACHE_KEY,
+                value: result,
+                options: new CacheOptions(Ttl: TimeSpan.FromMinutes(10))
+            );
+
+        return result;
+    }
+
 }
