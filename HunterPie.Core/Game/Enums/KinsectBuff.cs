9356d45ef9c4c878d0b76aaf52b60888aa86e096
haato3o <me@haato.dev>
2023-04-02 21:11:49 -0300

    Feat(UI): Added view model

---
+++ HunterPie.Core/Game/Enums/KinsectBuff.cs
@@ -0,0 +1,8 @@
+﻿namespace HunterPie.Core.Game.Enums;
+public enum KinsectBuff
+{
+    Attack,
+    Speed,
+    Defense,
+    None
+}
---
+++ HunterPie.UI/Overlay/Widgets/Classes/ViewModels/InsectGlaiveViewModel.cs
@@ -0,0 +1,25 @@
+﻿using HunterPie.Core.Game.Enums;
+using HunterPie.UI.Architecture;
+
+namespace HunterPie.UI.Overlay.Widgets.Classes.ViewModels;
+
+public class InsectGlaiveViewModel : ViewModel
+{
+    private KinsectBuff _primaryQueuedBuff;
+    public KinsectBuff PrimaryQueuedBuff { get => _primaryQueuedBuff; set => SetValue(ref _primaryQueuedBuff, value); }
+
+    private KinsectBuff _secondaryQueuedBuff;
+    public KinsectBuff SecondaryQueuedBuff { get => _secondaryQueuedBuff; set => SetValue(ref _secondaryQueuedBuff, value); }
+
+    private double _stamina;
+    public double Stamina { get => _stamina; set => SetValue(ref _stamina, value); }
+
+    private double _attackTimer;
+    public double AttackTimer { get => _attackTimer; set => SetValue(ref _attackTimer, value); }
+
+    private double _movementSpeedTimer;
+    public double MovementSpeedTimer { get => _movementSpeedTimer; set => SetValue(ref _movementSpeedTimer, value); }
+
+    private double _defenseTimer;
+    public double DefenseTimer { get => _defenseTimer; set => SetValue(ref _defenseTimer, value); }
+}
