b4787d2394a33e4bc6ed81df61aca7bee4a2d4be
haato3o <me@haato.dev>
2023-04-20 22:15:57 -0300

    Feat(Core): Mapped new monsters and fixed some minor offsets

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.8.0.32")]
-[assembly: AssemblyFileVersion("2.8.0.32")]
+[assembly: AssemblyVersion("2.8.0.36")]
+[assembly: AssemblyFileVersion("2.8.0.36")]
