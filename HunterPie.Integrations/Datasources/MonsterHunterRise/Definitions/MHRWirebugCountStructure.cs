d94b45f8b68e9092278d90ce3f2e7f84575b8031
WetU <50101064+WetU@users.noreply.github.com>
2023-04-25 12:16:43 +0900

    Hotfix(Core): Added IsTemporary check for extra timer update (#421)
    * Hotfix(Core): Added IsTemporary check for extra timer update
    
    * Refactor: Optimized reading passes and refactored code
    
    ---------
    
    Co-authored-by: haato3o <me@haato.dev>

---
+++ HunterPie.Integrations/Datasources/MonsterHunterRise/Definitions/MHRWirebugCountStructure.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HunterPie.Integrations.Datasources.MonsterHunterRise.Definitions;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct MHRWirebugCountStructure
+{
+    public int Default;
+    public int Environment;
+    public int Skill;
+
+    public int Total() => Math.Max(0, Default + Environment + Skill);
+
+    public bool AnyEnvironment() => Environment > 0;
+
+    public bool AnySkill() => Skill > 0;
+}
