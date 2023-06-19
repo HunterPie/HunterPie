14f2cfb4a24b9b3487bfe227e7e839e2d6005e02
haato3o <me@haato.dev>
2023-06-18 21:56:49 -0300

    Feat(UI): Finished Charge Blade helper

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.9.0.92")]
-[assembly: AssemblyFileVersion("2.9.0.92")]
+[assembly: AssemblyVersion("2.9.0.93")]
+[assembly: AssemblyFileVersion("2.9.0.93")]
