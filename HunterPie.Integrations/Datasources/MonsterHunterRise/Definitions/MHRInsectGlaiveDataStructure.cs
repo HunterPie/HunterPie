49f4a334a0f7d9ecb06c90a4bdaa6ba988d59cbd
haato3o <me@haato.dev>
2023-06-21 02:51:17 -0300

    Fix(Core): Fixed Insect Glaive offsets

--- HunterPie.Integrations/Datasources/MonsterHunterRise/Definitions/MHRInsectGlaiveDataStructure.cs
+++ HunterPie.Integrations/Datasources/MonsterHunterRise/Definitions/MHRInsectGlaiveDataStructure.cs
@@ -5,8 +5,8 @@ namespace HunterPie.Integrations.Datasources.MonsterHunterRise.Definitions;
 [StructLayout(LayoutKind.Explicit)]
 public struct MHRInsectGlaiveDataStructure
 {
-    [FieldOffset(0x9F8)] public long ExtractsArray;
-    [FieldOffset(0xA34)] public float AttackTimer;
-    [FieldOffset(0xA38)] public float SpeedTimer;
-    [FieldOffset(0xA3C)] public float DefenseTimer;
+    [FieldOffset(0xA08)] public long ExtractsArray;
+    [FieldOffset(0xA44)] public float AttackTimer;
+    [FieldOffset(0xA48)] public float SpeedTimer;
+    [FieldOffset(0xA4C)] public float DefenseTimer;
 }
