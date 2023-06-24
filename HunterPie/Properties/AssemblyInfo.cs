40ee53d9a91422c11d593b2abc042a0f69737006
haato3o <me@haato.dev>
2023-06-24 20:48:16 -0300

    Feat(UI): Changed settings description to be under it's name

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.9.0.103")]
-[assembly: AssemblyFileVersion("2.9.0.103")]
+[assembly: AssemblyVersion("2.9.0.108")]
+[assembly: AssemblyFileVersion("2.9.0.108")]
