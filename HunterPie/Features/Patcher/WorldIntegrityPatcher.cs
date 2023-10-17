611af350efed65d319204525cbda35be0400e503
haato3o <me@haato.dev>
2023-10-17 15:55:28 -0300

    Feat(MHW): Added support to MHW build 421631

--- HunterPie/Features/Patcher/WorldIntegrityPatcher.cs
+++ HunterPie/Features/Patcher/WorldIntegrityPatcher.cs
@@ -1,7 +1,5 @@
-﻿using HunterPie.Core.Address.Map;
-using HunterPie.Core.Game;
+﻿using HunterPie.Core.Game;
 using HunterPie.Core.Logger;
-using System;
 
 namespace HunterPie.Features.Patcher;
 
@@ -10,30 +8,6 @@ internal static class WorldIntegrityPatcher
 
     public static void Patch(IContext context)
     {
-        PatchCrc("CRC_FUNC_1A", "CRC_FUNC_1B", "CRC_ORIGINAL_FUNC_1A", "CRC_ORIGINAL_FUNC_1B");
-        PatchCrc("CRC_FUNC_2A", "CRC_FUNC_2B", "CRC_ORIGINAL_FUNC_2A", "CRC_ORIGINAL_FUNC_2B");
-
-        void PatchCrc(string addressName1, string addressName2, string originalFuncName1, string originalFuncName2)
-        {
-            long crc1 = AddressMap.GetAbsolute(addressName1);
-            long crc2 = AddressMap.GetAbsolute(addressName2);
-            if (!context.Process.Memory.ContentEquals(crc1, originalFuncName1)
-                || !context.Process.Memory.ContentEquals(crc2, originalFuncName2))
-                return;
-
-            const int jmpInstructionLength = 1 + 4;
-            byte[] patch = new byte[jmpInstructionLength + 1];
-
-            // JMP rel32: Jump near, relative, RIP = RIP + 32-bit displacement sign extended to 64-bits
-            patch[0] = 0xE9;
-            int rel32 = (int)(crc2 - crc1 - jmpInstructionLength);
-
-            BitConverter.TryWriteBytes(patch.AsSpan(1, 4), rel32);
-
-            // NOP
-            patch[jmpInstructionLength + 0] = 0x90;
-            context.Process.Memory.InjectAsm(crc1, patch);
-            Log.Debug("Patched 0x{0:X}", crc1);
-        }
+        Log.Warn("Make sure you have Stracker's Loader and it's dependencies installed.\nIf your game crash while using HunterPie, that means you don't have them installed.");
     }
 }
--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.9.0.122")]
-[assembly: AssemblyFileVersion("2.9.0.122")]
+[assembly: AssemblyVersion("2.9.0.124")]
+[assembly: AssemblyFileVersion("2.9.0.124")]
