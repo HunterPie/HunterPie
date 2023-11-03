54ae43a5323310971c4842328ef2813da0b6c7a3
haato3o <me@haato.dev>
2023-11-02 22:30:58 -0300

    Hotfix: Fixed email validation rule

--- HunterPie.UI/Architecture/Validation/EmailValidationRule.cs
+++ HunterPie.UI/Architecture/Validation/EmailValidationRule.cs
@@ -4,22 +4,44 @@ using System.Text.RegularExpressions;
 using System.Windows.Controls;
 
 namespace HunterPie.UI.Architecture.Validation;
+
+#nullable enable
 public class EmailValidationRule : ValidationRule
 {
-    private const string EMAIL_PATTERN = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*@((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
-
+    private const string EMAIL_FORMAT = @"(@)(.+)$";
+    private const string EMAIL_PATTERN = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
     private const string INVALID_EMAIL = "VALIDATION_FOR_EMAIL_FAIL";
 
-    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    private static string DomainMapper(Match match)
+    {
+        var idnMapping = new IdnMapping();
+
+        string domainName = idnMapping.GetAscii(match.Groups[2].Value);
+
+        return match.Groups[1].Value + domainName;
+    }
+
+    public override ValidationResult Validate(object? value, CultureInfo cultureInfo)
     {
         if (value is not string email)
             throw new ArgumentException("value must be string");
 
-        if (string.IsNullOrEmpty(email))
+        if (string.IsNullOrWhiteSpace(email))
             return new ValidationResult(true, INVALID_EMAIL);
 
-        bool isValid = Regex.IsMatch(email, EMAIL_PATTERN);
+        try
+        {
+            string normalizedEmail = Regex.Replace(email, EMAIL_FORMAT, DomainMapper, RegexOptions.None,
+                TimeSpan.FromMilliseconds(200));
 
-        return new ValidationResult(isValid, isValid ? null : INVALID_EMAIL);
+            bool isValid = Regex.IsMatch(normalizedEmail, EMAIL_PATTERN, RegexOptions.IgnoreCase,
+                TimeSpan.FromMilliseconds(200));
+
+            return new ValidationResult(isValid, isValid ? null : INVALID_EMAIL);
+        }
+        catch
+        {
+            return new ValidationResult(false, INVALID_EMAIL);
+        }
     }
 }
