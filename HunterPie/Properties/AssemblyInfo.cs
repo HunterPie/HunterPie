065d1fc6a10fbfb53dee459fa7f3cf929be69c33
haato3o <me@haato.dev>
2023-12-01 01:49:44 -0300

    Feat(UI): Replace hardcoded texts with propert localizations

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.10.0.55")]
-[assembly: AssemblyFileVersion("2.10.0.55")]
+[assembly: AssemblyVersion("2.10.0.59")]
+[assembly: AssemblyFileVersion("2.10.0.59")]
