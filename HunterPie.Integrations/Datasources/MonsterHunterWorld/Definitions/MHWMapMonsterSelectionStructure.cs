3a2c269d3cbdd1fda90cd2de954a86bec63ea2e4
haato3o <me@haato.dev>
2024-01-06 13:50:29 -0300

    Hotfix(Core): fixed map pin for MHW

--- HunterPie.Integrations/Datasources/MonsterHunterWorld/Definitions/MHWMapMonsterSelectionStructure.cs
+++ HunterPie.Integrations/Datasources/MonsterHunterWorld/Definitions/MHWMapMonsterSelectionStructure.cs
@@ -7,4 +7,5 @@ public struct MHWMapMonsterSelectionStructure
 {
     [FieldOffset(0x128)] public long MapInsectsRef;
     [FieldOffset(0x148)] public long SelectedMonster;
+    [FieldOffset(0x160)] public long GuiRadarRef;
 }
