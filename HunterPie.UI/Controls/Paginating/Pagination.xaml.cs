123cb4800bea2b70afa1bb18464c1c391e6ac8f7
haato3o <me@haato.dev>
2024-01-20 13:08:15 -0300

    Feat(UI): Create component

---
+++ HunterPie.UI/Controls/Paginating/Pagination.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HunterPie.UI.Controls.Paginating;
+/// <summary>
+/// Interaction logic for Pagination.xaml
+/// </summary>
+public partial class Pagination : UserControl
+{
+    public int TotalPages
+    {
+        get => (int)GetValue(TotalPagesProperty);
+        set => SetValue(TotalPagesProperty, value);
+    }
+
+    public static readonly DependencyProperty TotalPagesProperty =
+        DependencyProperty.Register(nameof(TotalPages), typeof(int), typeof(Pagination), new PropertyMetadata(0));
+
+    public Pagination()
+    {
+        InitializeComponent();
+    }
+}
