61b190b70d1ef17850ca86c971818c26c6b0457e
haato3o <me@haato.dev>
2024-01-21 22:23:08 -0300

    Feat(UI): Implemented pagination component

--- HunterPie.UI/Controls/Paginating/Pagination.xaml.cs
+++ HunterPie.UI/Controls/Paginating/Pagination.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using HunterPie.UI.Controls.Paginating.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace HunterPie.UI.Controls.Paginating;
@@ -7,6 +11,8 @@ namespace HunterPie.UI.Controls.Paginating;
 /// </summary>
 public partial class Pagination : UserControl
 {
+    public ObservableCollection<PaginationItemViewModel> Pages { get; } = new();
+
     public int TotalPages
     {
         get => (int)GetValue(TotalPagesProperty);
@@ -14,10 +20,57 @@ public partial class Pagination : UserControl
     }
 
     public static readonly DependencyProperty TotalPagesProperty =
-        DependencyProperty.Register(nameof(TotalPages), typeof(int), typeof(Pagination), new PropertyMetadata(0));
+        DependencyProperty.Register(nameof(TotalPages), typeof(int), typeof(Pagination), new PropertyMetadata(0, OnTotalPagesChange));
+
+    public int CurrentPage
+    {
+        get => (int)GetValue(CurrentPageProperty);
+        set => SetValue(CurrentPageProperty, value);
+    }
+
+    public static readonly DependencyProperty CurrentPageProperty =
+        DependencyProperty.Register(nameof(CurrentPage), typeof(int), typeof(Pagination), new PropertyMetadata(0));
 
     public Pagination()
     {
         InitializeComponent();
+    }
+
+    private static void OnTotalPagesChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not Pagination pagination)
+            return;
+
+        int totalPages = pagination.TotalPages;
+        int currentPage = pagination.CurrentPage;
+
+        Span<int> availablePages = stackalloc int[5] { 0, currentPage - 1, currentPage, currentPage + 1, totalPages - 1 };
+
+        List<PaginationItemViewModel> pageBuffer = new(Math.Min(7, totalPages));
+        int lastPageAdded = int.MinValue;
+        foreach (int page in availablePages)
+        {
+            if (page < 0 || page == lastPageAdded)
+                continue;
+
+            bool hasSkippedPages = page - lastPageAdded > 1;
+
+            if (hasSkippedPages)
+                pageBuffer.Add(new PaginationItemViewModel
+                {
+                    IsGap = true
+                });
+
+            pageBuffer.Add(new PaginationItemViewModel
+            {
+                IsActive = page == currentPage,
+                Page = page + 1
+            });
+
+            lastPageAdded = page;
+        }
+
+        pagination.Pages.Clear();
+        pageBuffer.ForEach(it => pagination.Pages.Add(it));
     }
 }
---
+++ HunterPie.UI/Controls/Paginating/ViewModels/PaginationItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using HunterPie.UI.Architecture;
+
+namespace HunterPie.UI.Controls.Paginating.ViewModels;
+
+public class PaginationItemViewModel : ViewModel
+{
+    private int _page;
+    public int Page { get => _page; set => SetValue(ref _page, value); }
+
+    private bool _isActive;
+    public bool IsActive { get => _isActive; set => SetValue(ref _isActive, value); }
+
+    private bool _isGap;
+    public bool IsGap { get => _isGap; set => SetValue(ref _isGap, value); }
+}
