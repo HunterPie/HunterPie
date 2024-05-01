f44af2d825c103384ec032deabcc742d6571ecd4
haato3o <me@haato.dev>
2024-05-01 12:05:28 -0300

    Chore: Bump minor version

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.11.0.1")]
-[assembly: AssemblyFileVersion("2.11.0.1")]
+[assembly: AssemblyVersion("2.11.0.2")]
+[assembly: AssemblyFileVersion("2.11.0.2")]
