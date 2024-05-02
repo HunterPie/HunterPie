e261468cd554db140eb0981bcb3b7cfdc6f8fd5f
Matheus Rangel <me@haato.dev>
2024-05-02 01:05:09 -0300

    Fix(UI): Hide Piercing Bind gauge for MHW (#517)

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 )]
 
 
-[assembly: AssemblyVersion("2.11.0.3")]
-[assembly: AssemblyFileVersion("2.11.0.3")]
+[assembly: AssemblyVersion("2.11.0.37")]
+[assembly: AssemblyFileVersion("2.11.0.37")]
