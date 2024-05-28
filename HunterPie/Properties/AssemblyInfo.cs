69b3ef1e7ce618cc6635a357aa639f58e9c00d7a
haato3o <me@haato.dev>
2024-05-28 20:32:25 -0300

    Fix(UI): Disable DPI awareness

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 
 using System.Reflection;
 using System.Windows;
+using System.Windows.Media;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -10,7 +11,7 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+[assembly: DisableDpiAwareness]
 
-
-[assembly: AssemblyVersion("2.11.0.49")]
-[assembly: AssemblyFileVersion("2.11.0.49")]
+[assembly: AssemblyVersion("2.11.0.53")]
+[assembly: AssemblyFileVersion("2.11.0.53")]
