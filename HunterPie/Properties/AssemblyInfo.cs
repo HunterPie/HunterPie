564e7ed3d5a32deadc721b86eed8cb86add1d52d
haato3o <me@haato.dev>
2024-06-04 22:20:44 -0300

    Feat(Core): Update addresses for build 421740

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Windows.Media;
 )]
 [assembly: DisableDpiAwareness]
 
-[assembly: AssemblyVersion("2.10.0.135")]
-[assembly: AssemblyFileVersion("2.10.0.135")]
+[assembly: AssemblyVersion("2.11.0.54")]
+[assembly: AssemblyFileVersion("2.11.0.54")]
