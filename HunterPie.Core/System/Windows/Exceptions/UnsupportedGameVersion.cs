814390e169f9b590e4f1930db8338a99409b8efe
samjviana <sam.jviana@gmail.com>
2024-06-22 15:23:37 -0300

    Updated `add-support-to-gamepass` to v2.11.0

---
+++ HunterPie.Core/System/Windows/Exceptions/UnsupportedGameVersion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HunterPie.Core.System.Windows.Exceptions;
+
+internal class UnsupportedGameVersion : Exception
+{
+    public UnsupportedGameVersion()
+        : base("The game version is not supported")
+    {
+
+    }
+}
