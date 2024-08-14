ca62f0f440c532d05e1e4ca4be287ce4275784ea
ssc19940105 <ssc19940105@users.noreply.github.com>
2024-08-14 11:59:55 +0800

    Fixed An Unexpected Behavior In SideBar When Account Was Not Logged In (#530)
    * Fix(UI): Fix SideBar scaling for high DPI displays (#527)
    
    Co-authored-by: 沈舒超 <ssc19940105@163.com>
    
    * Chore: Bump version
    
    * Add And Implement INavigator.ReturnWhen(TViewModel) Interface.
    
    * Fixed An Unexpected Behavior In SideBar When Account Was Not Logged In
    
    ---------
    
    Co-authored-by: haato3o <me@haato.dev>

--- HunterPie/UI/SideBar/Views/SideBarView.xaml.cs
+++ HunterPie/UI/SideBar/Views/SideBarView.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Windows;
+﻿using HunterPie.Core.Architecture;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using ResourceService = HunterPie.UI.Assets.Application.Resources;
 
 namespace HunterPie.UI.SideBar.Views;
 
@@ -13,49 +9,22 @@ namespace HunterPie.UI.SideBar.Views;
 /// </summary>
 public partial class SideBarView : UserControl
 {
-    private static readonly double SidebarWidth = ResourceService.Get<double>("HUNTERPIE_SIDEBAR_WIDTH");
-    private static readonly Duration AnimationDuration = new Duration(TimeSpan.FromMilliseconds(300));
-    private static readonly DoubleAnimation OpenSideBarAnimation =
-        new(SidebarWidth, AnimationDuration)
-        {
-            EasingFunction = new QuadraticEase()
-        };
-
-    private static readonly DoubleAnimation CloseSideBarAnimation =
-        new(50.0, AnimationDuration)
-        {
-            EasingFunction = new QuadraticEase()
-        };
+    public Observable<bool> IsMouseInside { get; } = false;
 
     public SideBarView()
     {
         InitializeComponent();
     }
 
-    private void OnPreviewMouseMove(object sender, MouseEventArgs e)
-    {
-        Point sideBarPosition = e.GetPosition(PART_SideBar);
-        HitTestResult? sideBarHitTestResult = VisualTreeHelper.HitTest(PART_SideBar, sideBarPosition);
-        bool isMouseOverSideBar = sideBarHitTestResult is not null && PART_SideBar.IsHitTestVisible;
-
-        Point hitTestPosition = e.GetPosition(PART_HitTest);
-        HitTestResult? maskHitTestResult = VisualTreeHelper.HitTest(PART_HitTest, hitTestPosition);
-        bool isMouseOverMask = maskHitTestResult is not null;
-        bool isMouseOver = isMouseOverMask || isMouseOverSideBar;
-        PART_SideBar.IsHitTestVisible = isMouseOver;
-
-        AnimateMask(isMouseOver);
-    }
-
     private void OnMouseLeave(object sender, MouseEventArgs e)
     {
+        IsMouseInside.Value = false;
         PART_SideBar.IsHitTestVisible = false;
-        AnimateMask(false);
     }
 
-    private void AnimateMask(bool isMouseOver)
+    private void OnMouseEnter(object sender, MouseEventArgs e)
     {
-        DoubleAnimation animation = isMouseOver ? OpenSideBarAnimation : CloseSideBarAnimation;
-        PART_HitTest.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, animation, HandoffBehavior.SnapshotAndReplace);
+        IsMouseInside.Value = true;
+        PART_SideBar.IsHitTestVisible = true;
     }
 }
