6e6dcd0ee936b1c7706a8e02d3fd0b4d7bb9259b
Matheus Rangel <me@haato.dev>
2024-09-15 22:46:34 -0300

    Fix(UI): Dispose clock widget on game exit (#548)

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.11.0.119")]
-[assembly: AssemblyFileVersion("2.11.0.119")]
+[assembly: AssemblyVersion("2.11.0.120")]
+[assembly: AssemblyFileVersion("2.11.0.120")]
