f9c4b5874fc2663ea734727230add6d38c1ee668
Haato3o <me@haato.dev>
2025-03-14 16:08:51 +0100

    fix(ui): hide monsters in compact mode when target only is enabled

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.12.0.465")]
-[assembly: AssemblyFileVersion("2.12.0.465")]
+[assembly: AssemblyVersion("2.12.0.469")]
+[assembly: AssemblyFileVersion("2.12.0.469")]
