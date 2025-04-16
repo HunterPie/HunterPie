f212443fdab9d62beaaaeb88efc641b5b3b057a5
Haato3o <me@haato.dev>
2025-04-16 13:27:43 +0200

    feat(core): mapped new addresses

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.12.0.798")]
-[assembly: AssemblyFileVersion("2.12.0.798")]
+[assembly: AssemblyVersion("2.12.0.799")]
+[assembly: AssemblyFileVersion("2.12.0.799")]
