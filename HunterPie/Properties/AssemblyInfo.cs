4235182485cb364b89e5ccf2d2e0dc6ba252daf8
Haato3o <me@haato.dev>
2025-04-17 14:02:22 +0200

    feat(data): mapped monsters extra parts

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.12.0.799")]
-[assembly: AssemblyFileVersion("2.12.0.799")]
+[assembly: AssemblyVersion("2.12.0.800")]
+[assembly: AssemblyFileVersion("2.12.0.800")]
