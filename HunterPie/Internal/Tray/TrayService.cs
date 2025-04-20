7d6b389cd4c0c66b6123e3075153db39b660bc2e
Haato3o <me@haato.dev>
2025-04-20 23:18:21 +0200

    fix(core): fixed null exception during auto-update

--- HunterPie/Internal/Tray/TrayService.cs
+++ HunterPie/Internal/Tray/TrayService.cs
@@ -5,9 +5,9 @@ namespace HunterPie.Internal.Tray;
 
 public class TrayService
 {
-    private readonly ContextMenuStrip contextMenuStrip = new();
-    private readonly NotifyIcon notifyIcon = new();
-    private static TrayService _instance;
+    private readonly ContextMenuStrip _contextMenuStrip = new();
+    private readonly NotifyIcon _notifyIcon = new();
+    private static TrayService? _instance;
 
     private TrayService(
         string tooltip,
@@ -15,33 +15,42 @@ public class TrayService
         Icon icon
     )
     {
-        notifyIcon.BalloonTipTitle = tooltip;
-        notifyIcon.Text = text;
-        notifyIcon.Icon = icon;
-        notifyIcon.Visible = true;
-        notifyIcon.ContextMenuStrip = contextMenuStrip;
+        _notifyIcon.BalloonTipTitle = tooltip;
+        _notifyIcon.Text = text;
+        _notifyIcon.Icon = icon;
+        _notifyIcon.Visible = true;
+        _notifyIcon.ContextMenuStrip = _contextMenuStrip;
     }
 
     internal static void Initialize(
         string tooltip,
         string text,
         Icon icon
-    ) => _instance = new(tooltip, text, icon);
+    ) => _instance = new TrayService(tooltip, text, icon);
 
     internal static void Dispose()
     {
         if (_instance is null)
             return;
 
-        _instance.contextMenuStrip.Dispose();
-        _instance.notifyIcon.Dispose();
+        _instance._contextMenuStrip.Dispose();
+        _instance._notifyIcon.Dispose();
 
         _instance = null;
     }
 
-    public static void AddDoubleClickHandler(MouseEventHandler callback) => _instance.notifyIcon.MouseDoubleClick += callback;
+    public static void AddDoubleClickHandler(MouseEventHandler callback)
+    {
+        if (_instance is null)
+            return;
 
-    public static ToolStripMenuItem AddItem(string name) => _instance.contextMenuStrip.Items.Add(name) as ToolStripMenuItem;
+        _instance._notifyIcon.MouseDoubleClick += callback;
+    }
 
-    public static ToolStripMenuItem AddMenuItem(string name, ToolStripMenuItem menu) => menu.DropDownItems.Add(name) as ToolStripMenuItem;
+    public static ToolStripMenuItem? AddItem(string name)
+    {
+        return _instance?._contextMenuStrip.Items.Add(name) as ToolStripMenuItem;
+    }
+
+    public static ToolStripMenuItem? AddMenuItem(string name, ToolStripMenuItem menu) => menu.DropDownItems.Add(name) as ToolStripMenuItem;
 }
