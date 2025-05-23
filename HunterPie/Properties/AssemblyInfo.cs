ad4d8b49e62182cc4ad512fea4c3e042455ce70b
haato3o <me@haato.dev>
2025-05-22 23:02:07 -0300

    feat(ui): display full damage number

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.12.1.3")]
-[assembly: AssemblyFileVersion("2.12.1.3")]
+[assembly: AssemblyVersion("2.12.1.6")]
+[assembly: AssemblyFileVersion("2.12.1.6")]
