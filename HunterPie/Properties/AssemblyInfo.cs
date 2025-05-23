65a69d34ff77f2fc7de623f708853845e98280e3
haato3o <me@haato.dev>
2025-05-22 22:52:53 -0300

    feat(core): added synchronized damage value as the main source

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.12.0.876")]
-[assembly: AssemblyFileVersion("2.12.0.876")]
+[assembly: AssemblyVersion("2.12.1.3")]
+[assembly: AssemblyFileVersion("2.12.1.3")]
