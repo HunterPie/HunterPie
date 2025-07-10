b5e6835c5cfe6af7c22f482a55f4be4c09b0ce7e
haato3o <me@haato.dev>
2025-07-09 23:22:20 -0300

    feat(data): updated offsets

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.12.1.35")]
-[assembly: AssemblyFileVersion("2.12.1.35")]
+[assembly: AssemblyVersion("2.12.1.36")]
+[assembly: AssemblyFileVersion("2.12.1.36")]
