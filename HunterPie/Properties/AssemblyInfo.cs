92686bd429752ccb09d03786ae05aad104468d3f
haato3o <me@haato.dev>
2025-08-14 12:39:55 -0300

    feat(core): added support to mhwilds v1.21.1.0

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.12.1.41")]
-[assembly: AssemblyFileVersion("2.12.1.41")]
+[assembly: AssemblyVersion("2.12.1.43")]
+[assembly: AssemblyFileVersion("2.12.1.43")]
