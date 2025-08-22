cb5c22f4c150b63c328584abbac471f3a61229aa
haato3o <me@haato.dev>
2025-08-21 23:22:49 -0300

    feat(core): add missing addresses for v1.21.3.0

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.13.1.254")]
-[assembly: AssemblyFileVersion("2.13.1.254")]
+[assembly: AssemblyVersion("2.13.1.255")]
+[assembly: AssemblyFileVersion("2.13.1.255")]
