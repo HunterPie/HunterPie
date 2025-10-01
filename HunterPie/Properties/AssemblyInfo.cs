7ca41634c40e3b0599c1d5bc7e219d227cd0ca5b
haato3o <me@haato.dev>
2025-09-30 23:28:18 -0300

    feat(core): add missing offsets

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.13.1.264")]
-[assembly: AssemblyFileVersion("2.13.1.264")]
+[assembly: AssemblyVersion("2.13.1.265")]
+[assembly: AssemblyFileVersion("2.13.1.265")]
