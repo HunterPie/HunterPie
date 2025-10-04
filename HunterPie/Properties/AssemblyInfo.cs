a7d33d86ff218a43f809d4fae0e5128d227215d6
Matheus Rangel <me@haato.dev>
2025-10-04 00:07:29 -0300

    fix(core): mhwilds abnormalities v1.30.1.0 (#699)
    * fix(core): fix abnormalities offsets
    
    * chore: localization submodule

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.12.1.71")]
-[assembly: AssemblyFileVersion("2.12.1.71")]
+[assembly: AssemblyVersion("2.12.1.75")]
+[assembly: AssemblyFileVersion("2.12.1.75")]
