36ecf1d57b6b874c3bf007d2e8dc7c05659cd6e0
Matheus Rangel <me@haato.dev>
2025-11-25 00:36:30 -0300

    fix(ui): fix null exception when clearing collection (#725)

--- HunterPie/Integrations/Poogie/Report/Models/CrashReportContextRequest.cs
+++ HunterPie/Integrations/Poogie/Report/Models/CrashReportContextRequest.cs
@@ -5,5 +5,6 @@ namespace HunterPie.Integrations.Poogie.Report.Models;
 public record CrashReportContextRequest(
     [property: JsonProperty("ram_total")] ulong RamTotal,
     [property: JsonProperty("ram_used")] long RamUsed,
-    [property: JsonProperty("windows_version")] string WindowsVersion
+    [property: JsonProperty("windows_version")] string WindowsVersion,
+    [property: JsonProperty("session_time")] string SessionTime
 );
