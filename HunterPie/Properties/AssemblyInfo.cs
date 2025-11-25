f2fd93e84efaede2340d5c2c3b3cec7cb0e9ab08
Matheus Rangel <me@haato.dev>
2025-11-24 22:49:55 -0300

    fix(data): fixed missing ailments (#724)

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.13.1.828")]
-[assembly: AssemblyFileVersion("2.13.1.828")]
+[assembly: AssemblyVersion("2.13.1.831")]
+[assembly: AssemblyFileVersion("2.13.1.831")]
