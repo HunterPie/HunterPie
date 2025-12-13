fb0283b9e596c33a501cfc86da199084afd44395
haato3o <me@haato.dev>
2025-12-13 17:05:02 -0300

    fix(data): fixed monster data file

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.14.0.172")]
-[assembly: AssemblyFileVersion("2.14.0.172")]
+[assembly: AssemblyVersion("2.14.0.175")]
+[assembly: AssemblyFileVersion("2.14.0.175")]
