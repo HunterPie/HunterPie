f46b9ff91fd07801c41fa9bdc1dc8f5dde22b4bf
haato3o <me@haato.dev>
2025-12-16 13:19:20 -0300

    fix(core): added partial support to v1.40.0.0

--- HunterPie.Integrations/Datasources/MonsterHunterWilds/Definitions/Player/MHWildsPlayerGearContext.cs
+++ HunterPie.Integrations/Datasources/MonsterHunterWilds/Definitions/Player/MHWildsPlayerGearContext.cs
@@ -8,5 +8,5 @@ public struct MHWildsPlayerGearContext
 {
     [FieldOffset(0x38)]
     [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] Gear;
-    [FieldOffset(0xBC)] public Weapon WeaponId;
+    [FieldOffset(0xC8)] public Weapon WeaponId;
 }
