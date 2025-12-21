3827a4b783894fd041e323c039979405ca1949f5
haato3o <me@haato.dev>
2025-12-21 13:28:44 -0300

    fix(core): fixed most common exceptions

--- HunterPie.Core/Converters/SecretConverter.cs
+++ HunterPie.Core/Converters/SecretConverter.cs
@@ -1,4 +1,5 @@
-﻿using HunterPie.Core.Settings.Types;
+﻿using HunterPie.Core.Observability.Logging;
+using HunterPie.Core.Settings.Types;
 using Newtonsoft.Json;
 using System;
 using System.Linq;
@@ -7,16 +8,25 @@ namespace HunterPie.Core.Converters;
 
 public class SecretConverter : JsonConverter
 {
+    private readonly ILogger _logger = LoggerFactory.Create();
+
     public override bool CanConvert(Type objectType) => objectType.GetInterfaces().Contains(typeof(Secret));
 
     public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
     {
-        string value = reader.Value != null
+        try
+        {
+            string value = reader.Value != null
             ? (string)Convert.ChangeType(reader.Value, typeof(string))
             : string.Empty;
 
-        objectType.GetProperty(nameof(Secret.EncryptedValue))?
-            .SetValue(existingValue, value);
+            objectType.GetProperty(nameof(Secret.EncryptedValue))?
+                .SetValue(existingValue, value);
+        }
+        catch (Exception err)
+        {
+            _logger.Error($"failed to deserialize secret: {err}");
+        }
 
         return existingValue;
     }
