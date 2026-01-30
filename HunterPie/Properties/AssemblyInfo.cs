19d48a059adcfc265a84d88bb5e50ef1a17f2410
Matheus Rangel <me@haato.dev>
2026-01-30 11:24:43 -0300

    [TRIVIAL] fix(core): add support to mhwilds v1.40.3.2 (#763)

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.13.1.891")]
-[assembly: AssemblyFileVersion("2.13.1.891")]
+[assembly: AssemblyVersion("2.13.1.893")]
+[assembly: AssemblyFileVersion("2.13.1.893")]
