dccbe5e65e3d0ecfbbdc6e3e3fb576ffee2b3195
haato3o <me@haato.dev>
2026-02-16 11:45:03 -0300

    feat(ui): change badge to tag control

---
+++ HunterPie.UI/Controls/Label/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HunterPie.UI.Controls.Label;
+
+public class Tag : Control
+{
+    public string Label
+    {
+        get => (string)GetValue(LabelProperty); set => SetValue(LabelProperty, value);
+    }
+    public static readonly DependencyProperty LabelProperty =
+        DependencyProperty.Register(nameof(Label), typeof(string), typeof(Tag));
+
+    static Tag()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(Tag), new FrameworkPropertyMetadata(typeof(Tag)));
+    }
+}
