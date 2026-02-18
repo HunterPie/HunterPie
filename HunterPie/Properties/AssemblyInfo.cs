d6d1dbf196b22fda3e2406a7c4558de7ee003353
haato3o <me@haato.dev>
2026-02-18 18:55:01 -0300

    fix(core): disable CETCompat to support Windows 10

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.14.0.427")]
-[assembly: AssemblyFileVersion("2.14.0.427")]
+[assembly: AssemblyVersion("2.14.0.429")]
+[assembly: AssemblyFileVersion("2.14.0.429")]
