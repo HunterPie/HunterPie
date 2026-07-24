eebbff72d2d7e5b3cb145cddcfc91b481c739e0c
haato3o <me@haato.dev>
2026-07-23 21:21:26 -0300

    chore: bump version

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.15.0.242")]
-[assembly: AssemblyFileVersion("2.15.0.242")]
+[assembly: AssemblyVersion("2.15.0.243")]
+[assembly: AssemblyFileVersion("2.15.0.243")]
