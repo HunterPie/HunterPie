1576058ca6867a6714aae75af1afc1504f4125da
haato3o <me@haato.dev>
2026-07-25 15:06:21 -0300

    refactor(ui): making sidebar look more compact and clean

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.15.0.246")]
-[assembly: AssemblyFileVersion("2.15.0.246")]
+[assembly: AssemblyVersion("2.15.0.254")]
+[assembly: AssemblyFileVersion("2.15.0.254")]
