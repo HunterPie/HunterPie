f79e4b340f638ab9421f97f1c4b5e3dc61a9b0f8
haato3o <me@haato.dev>
2026-08-04 23:27:38 -0300

    feat(ui): migrated other components to use the new accent token

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.15.0.383")]
-[assembly: AssemblyFileVersion("2.15.0.383")]
+[assembly: AssemblyVersion("2.15.0.385")]
+[assembly: AssemblyFileVersion("2.15.0.385")]
