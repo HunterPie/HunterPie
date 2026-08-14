ef654889658684848cb465176b676b9b553ea102
Matheus Rangel <me@haato.dev>
2026-08-14 09:04:05 -0300

    [TRIVIAL] fix(core): added support to monster hunter wilds v1.42.0.2 (#823)

--- HunterPie/Properties/AssemblyInfo.cs
+++ HunterPie/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
-[assembly: AssemblyVersion("2.14.0.465")]
-[assembly: AssemblyFileVersion("2.14.0.465")]
+[assembly: AssemblyVersion("2.14.0.466")]
+[assembly: AssemblyFileVersion("2.14.0.466")]
